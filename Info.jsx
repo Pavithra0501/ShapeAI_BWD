be74af5558b3aa32b8dbe0d92ebdcd472ca32f18
Pavithra0501 <76801349+Pavithra0501@users.noreply.github.com>
2021-06-26 19:42:43 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info (){
+  return(
+    <div className="note">
+      <h1>Java script and React.Js</h1>
+      <p>A basic web dev React Js Bootcamp</p>
+      </div>
+     
+  );
+}
+
+export default Info;
